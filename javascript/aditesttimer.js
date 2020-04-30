58de3367b9579db426edc92f5130fed850df6073
Adam Binnersley <AdamB7586@users.noreply.github.com>
2020-04-30 16:13:52 +0100

    Add javascript

---
+++ javascript/aditesttimer.js
@@ -0,0 +1,24 @@
+function countdown(seconds){
+    clearInterval(countdown);
+    if(seconds){var count = seconds;}else{var count = 5399;}
+    var countdown = setInterval(function(){
+        var time = secondstotime(count);
+        $("#time").html(time);
+        if(count == 0){
+            clearInterval(countdown);
+            endTest();
+        }
+        count--;
+    }, 1000);	
+}
+
+function secondstotime(secs){
+    var t = new Date(1970,0,1);
+    t.setSeconds(secs);
+    var s = t.toTimeString().substr(3,5);
+    return s;
+}
+
+$(document).ready(function(){
+    if(countstart !== false){countdown();}
+});
