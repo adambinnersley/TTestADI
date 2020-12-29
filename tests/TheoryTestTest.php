70adf5baa5c1fb2ec5de7115fec3a292477f1f43
Adam Binnersley <abinnersley@gmail.com>
2020-12-29 16:46:02 +0000

    Change from static definitions

--- tests/TheoryTestTest.php
+++ tests/TheoryTestTest.php
@@ -12,7 +12,7 @@ class TheoryTestTest extends SetUp
     protected function setUp() : void
     {
         parent::setUp();
-        $this->theoryTest = new TheoryTest(self::$db, self::$config, self::$template, self::$user);
+        $this->theoryTest = new TheoryTest($this->db, $this->config, $this->template, $this->user);
     }
     
     public function testExample()
